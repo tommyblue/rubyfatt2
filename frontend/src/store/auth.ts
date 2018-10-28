e5c27232a690c77faba7c8775b084453dee5fe21
Tommaso Visconti <tommaso.visconti@gmail.com>
2018-10-28 16:40:54 +0100

    Manage authToken and cookies

--- frontend/src/store/auth.ts
+++ frontend/src/store/auth.ts
@@ -1,16 +1,26 @@
-import { observable } from "mobx";
 import { isEmpty } from "lodash";
+import { observable } from "mobx";
+import Cookies from "universal-cookie";
 
 import { RootStore } from "./store";
 import { MessageTypes } from "./messages";
+
+interface IAuthResponse {
+    data: {
+        token: string;
+    }
+}
 
 export class AuthStore {
     rootStore: RootStore = null;
 
     @observable authToken: string | null = null;
+    cookie_name = "auth_token";
 
     constructor(rootStore: RootStore) {
         this.rootStore = rootStore;
+
+        this.checkCookie();
     }
 
     public authenticate(email: string, password: string): void {
@@ -24,17 +34,50 @@ export class AuthStore {
         fetch("/api/session/sign_in", {
             method: "POST",
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                "Accept": "application/json",
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({email, password}),
         }).then(
-            response => {
+            (response: any) => {
                 if (!response.ok) {
                     return console.log(response.statusText);
                 }
-                return response.json().then(jsonResp => console.log(jsonResp.data));
+                return response.json().then(
+                    (jsonResp: IAuthResponse) => {
+                        this.setAuthToken(jsonResp.data.token);
+                        this.authToken = jsonResp.data.token;
+                    }
+                );
             }
         );
+    }
+
+    public signOut(): void {
+        this.removeAuthToken();
+        this.authenticate = null;
+    }
+
+    private setAuthToken(token: string): void {
+        const cookies = new Cookies();
+        cookies.set(this.cookie_name, token, {
+            path: "/"
+        });
+    };
+
+    private removeAuthToken(): void {
+        const cookies = new Cookies();
+        cookies.remove(this.cookie_name, {
+            path: "/",
+        });
+    }
+
+    // Load cookie if it exists and set authToken
+    private checkCookie(): void {
+        const cookies = new Cookies();
+        const cookie = cookies.get(this.cookie_name)
+       if (cookie) {
+           this.authToken = cookie;
+       }
     }
 }
